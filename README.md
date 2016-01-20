19e137d303a0b7cb8b72df928e63b8649ec3df3a
chaipokoi <chaipokoi@gmail.com>
2016-01-20 19:49:41 +0100

    Finalisation du script, ajout d'un README

---
+++ README.md
@@ -0,0 +1,9 @@
+# TODOCounter
+
+## Description
+
+Un petit utilistaire qui parcours tout les fichiers d'un répertoire pour en trouver les lignes "TODO" est les afficher en format markdown dans le fichier README de votre projet.
+
+## Usage
+
+Placez le script TodoCounter.py à la racine de votre projet, est exécutez-le, les tâches seront ajoutées à la fin du ficher README, pensez donc à prévoir un espace pour les afficher dans ce dernier.
--- TodoCounter.py
+++ TodoCounter.py
@@ -1,20 +1,41 @@
+#coding: utf-8
+
 import sys
-import re 
+import re
+from os import listdir
+import os
+from os.path import isfile, join
 
-if len(sys.argv) < 2:
-	print "Vous devez preciser un fichier a analyser"
-	exit()
+matchs = [];
+files = [];
 
-try:
-	file = open(sys.argv[1],"r")
-except IOError: 
-	print "Impossible de lire le fichier a analyser."
-	exit()
+def listFiles(dir):
+	for f in listdir(dir):
+		if isfile(join(dir,f)) and sys.argv[0] not in f and "README" not in f and "readme" not in f:
+			files.append(join(dir,f))
+		elif sys.argv[0] not in f and "README" not in f and "readme" not in f:
+			listFiles(join(dir,f))
+
+listFiles("./")
+
+for f in files:
+	file = open(f,"r")
+	data = file.read()
+	res = re.findall("TODO:(.*?)\n", data)
+	lines = re.split("\n", data)
+	for i  in range(len(res)):
+		for u in range(len(lines)):
+			if res[i] in lines[u]:
+				res[i] = f+" l: "+str(u+1)+" "+res[i]
+				break
+	matchs = matchs + res;
+	file.close()
+
+#ecriture des  resultats
+file = open(os.path.join("./","README.md"), "r")
 data = file.read()
-matchs = re.findall("TODO:(.*?)\n", data)
 file.close()
-file = open("README.md", "a+")
-data = file.read()
+file = open(os.path.join("./","README.md"), "a+")
 for entry in matchs:
 	if entry not in data:
-		file.write("- [ ]:"+entry+"\n")
+		file.write("- [ ] "+entry+"\n")
