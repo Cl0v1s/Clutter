ab0788a9cecfefe14f8c3d6e81fbf6c08cce0340
chaipokoi <chaipokoi@gmail.com>
2016-03-16 11:44:53 +0100

    Test sur github

--- TodoCounter.py
+++ TodoCounter.py
@@ -30,7 +30,7 @@ for f in files:
 				print "    Entrée trouvée ligne "+str(u+1)
 				ft = f
 				#Eventuellement remplacer master par branche actuelle
-				ft = "blob/master"+ft.replace(".","", 1)
+				ft = ""+ft.replace(".","", 1)
 				res[i] = "["+f+"]("+ft+") l: "+str(u+1)+" "+res[i]
 				break
 	matchs = matchs + res;
